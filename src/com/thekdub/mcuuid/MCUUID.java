95eebe42ddc022c01ec80260703bc581a7852cae
TheKDub <blacktragonmc@gmail.com>
2020-03-14 22:38:45 -0400

    Command interface implemented. Logging is not currently working in this update.

--- src/com/thekdub/mcuuid/MCUUID.java
+++ src/com/thekdub/mcuuid/MCUUID.java
@@ -1,37 +1,22 @@
 package com.thekdub.mcuuid;
 
+import com.thekdub.mcuuid.exceptions.UUIDNotFoundException;
+import com.thekdub.mcuuid.exceptions.UserNotFoundException;
 import com.thekdub.mcuuid.utilities.DataStore;
 import com.thekdub.mcuuid.utilities.Logger;
 import com.thekdub.mcuuid.utilities.Parser;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MCUUID extends JavaPlugin {
   public static MCUUID instance;
   public long updateFrequency = 0L;
-
-  public static void main(String[] args) {
-    String uuid = "19d936397bb74a26b98fdae1b65e394d";
-    String name = "fadsfaswfe";
-    try {
-      System.out.println(UUIDAPI.getUUID(name));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    try {
-      System.out.println(UUIDAPI.getName(uuid));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    try {
-      System.out.println(UUIDAPI.getName(uuid));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 
   public void onEnable() {
     instance = this;
@@ -49,6 +34,46 @@ public class MCUUID extends JavaPlugin {
   }
 
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (cmd.getLabel().equalsIgnoreCase("mcuuid")) {
+      if (args.length == 0) { //Display How To Use
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&2MC&6UUID&8] &7"
+              + "Use &b/MCUUID (UUID) &7to find a user's name from their UUID"));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "    &7"
+              + "Use &b/MCUUID (name) &7to find a user's UUID from their name"));
+      }
+      else { //Process Arguments
+        String arg = args[0];
+        if (arg.length() <= 16) { //Username
+          if (Bukkit.getPlayer(arg) != null)
+            arg = Bukkit.getPlayer(arg).getName();
+          try {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&2MC&6UUID&8] &7"
+                  + "The UUID of the user '&b" + arg + "&7' is '&b" + UUIDAPI.getUUID(arg) + "&7'"));
+          }
+          catch (IOException e) {
+            e.printStackTrace();
+          }
+          catch (UserNotFoundException e) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&2MC&6UUID&8] &c"
+                  + e.getMessage()));
+          }
+        }
+        else { //UUID
+          try {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&2MC&6UUID&8] &7"
+                  + "The name of the UUID '&b" + arg + "&7' is '&b" + UUIDAPI.getName(arg) + "&7'"));
+          }
+          catch (IOException e) {
+            e.printStackTrace();
+          }
+          catch (UUIDNotFoundException e) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&2MC&6UUID&8] &c"
+                  + e.getMessage()));
+          }
+        }
+      }
+      return true;
+    }
     return false;
   }
 }
