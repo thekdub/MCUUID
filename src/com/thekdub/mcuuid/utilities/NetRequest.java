e1c4333fcafbfab3dd543f366f222e6ec549a1ef
TheKDub <blacktragonmc@gmail.com>
2020-05-28 16:54:01 -0400

    Not much to see here..
    utilities.NetRequest:
      Added log messages for when API access fails.
      Added failed API access cooldowns to prevent spamming of the API when inaccessible.
    
    MCUUID:
      Added a failedCooldown variable for reference in the utilities.NetRequest class.
    
    config.yml:
      Added a section for API Failure Cooldown control.
    
    plugin.yml:
      Version updated.

--- src/com/thekdub/mcuuid/utilities/NetRequest.java
+++ src/com/thekdub/mcuuid/utilities/NetRequest.java
@@ -1,5 +1,6 @@
 package com.thekdub.mcuuid.utilities;
 
+import com.thekdub.mcuuid.MCUUID;
 import com.thekdub.mcuuid.exceptions.UUIDNotFoundException;
 import com.thekdub.mcuuid.exceptions.UserNotFoundException;
 
@@ -8,11 +9,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
-public class NetRequest {
+class NetRequest {
+
+  private static HashMap<String, Long> failedCooldown = new HashMap<>();
+
   static String fetchUUID(String player, long ms) throws IOException, UserNotFoundException {
-    Logger.write("Fetching UUID for '" + player + "' from Mojang API.");
     player = player.replaceAll("[^a-zA-Z0-9_]", "");
+    if (failedCooldown.containsKey(player)) {
+      if (failedCooldown.get(player) + MCUUID.instance.failureCooldown > System.currentTimeMillis()) {
+        throw new UserNotFoundException(player);
+      }
+      else {
+        failedCooldown.remove(player);
+      }
+    }
+    Logger.write("Fetching UUID for '" + player + "' from Mojang API.");
     String url = "https://api.mojang.com/users/profiles/minecraft/" + player + "?at=" + ms;
     HttpURLConnection urlConnection = (HttpURLConnection) (new URL(url)).openConnection();
     urlConnection.setRequestMethod("GET");
@@ -23,14 +36,25 @@ public class NetRequest {
       result.append(line);
     }
     br.close();
-    if (result.length() == 0)
+    if (result.length() == 0) {
+      failedCooldown.put(player, System.currentTimeMillis());
+      Logger.write("Failed to fetch UUID for '" + player + "' from Mojang API.");
       throw new UserNotFoundException(player);
+    }
     return result.toString();
   }
 
   static String fetchNames(String uuid) throws IOException, UUIDNotFoundException {
-    Logger.write("Fetching name for '" + uuid + "' from Mojang API.");
     uuid = uuid.replaceAll("[^a-zA-Z0-9]", "");
+    if (failedCooldown.containsKey(uuid)) {
+      if (failedCooldown.get(uuid) + MCUUID.instance.failureCooldown > System.currentTimeMillis()) {
+        throw new UUIDNotFoundException(uuid);
+      }
+      else {
+        failedCooldown.remove(uuid);
+      }
+    }
+    Logger.write("Fetching name for '" + uuid + "' from Mojang API.");
     String url = "https://api.mojang.com/user/profiles/" + uuid + "/names";
     HttpURLConnection urlConnection = (HttpURLConnection) (new URL(url)).openConnection();
     urlConnection.setRequestMethod("GET");
@@ -41,8 +65,11 @@ public class NetRequest {
       result.append(line);
     }
     br.close();
-    if (result.length() == 0)
+    if (result.length() == 0) {
+      failedCooldown.put(uuid, System.currentTimeMillis());
+      Logger.write("Failed to fetch name for '" + uuid + "' from Mojang API.");
       throw new UUIDNotFoundException(uuid);
+    }
     return result.toString();
   }
 }
